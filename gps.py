c6dfe08cfcd5bb6774ebe281d2b4e04ffd8f9796
Sai Shibu <saishibu38380@gmail.com>
2018-05-10 13:48:12 +0530

    Update gps.py

--- gps.py
+++ gps.py
@@ -3,6 +3,7 @@ import serial,pynmea2,time,pymysql
 port = serial.Serial("/dev/ttyUSB0", baudrate=9600)
 conn=pymysql.connect(database="OceanNet",user="on",password="amma",host="localhost")
 cur=conn.cursor()
+BOAT='Sarveshwara_May10'
 a=1
 while a:
 	rcv = port.readline()
@@ -11,10 +12,10 @@ while a:
 		msg=pynmea2.parse(rcv)
 		lat=msg.lat
 		print lat
-		long=msg.lon
-		print long
-		data={'lat':lat,'long':long}
-		cur.execute("INSERT INTO GPS(Lat,Longi) VALUES (%(lat)s,%(long)s);",data)
+		lon=msg.lon
+		print lon
+		data={'BOAT':BOAT,'lat':lat,'long':long}
+		cur.execute("INSERT INTO GPS(BOAT,LAT,LON) VALUES (%(BOAT)s,%(lat)s,%(lon)s);",data)
 		conn.commit()
 		print "GPS Committed"
 		a=0
