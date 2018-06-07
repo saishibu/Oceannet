4114e886557dcf875aaec197a418c9f307ccafc7
Sai Shibu <saishibu38380@gmail.com>
2018-06-07 11:44:32 +0530

    LED Fix

--- nanostation.py
+++ nanostation.py
@@ -42,32 +42,37 @@ def statusled(cond):
 		GPIO.output(26,GPIO.LOW) #status led on
 #RSSI LED Config
 def rssiled(rssi):
-	#RSSI
+	print rssi
 	if rssi >= 80: #all ON
 		GPIO.output(5,GPIO.HIGH) #4
 		GPIO.output(6,GPIO.HIGH) #3
 		GPIO.output(13,GPIO.HIGH) #2
 		GPIO.output(19,GPIO.HIGH) #1
+		print "80+"
 	if 61 <= rssi <= 79: #RSSI3 off
 		GPIO.output(5,GPIO.HIGH) #4
 		GPIO.output(6,GPIO.HIGH) #3
 		GPIO.output(13,GPIO.HIGH) #2
 		GPIO.output(19,GPIO.LOW) #1
+		print "61to79"
 	if 41 <= rssi <= 60: #RSSI3&2 off
 		GPIO.output(5,GPIO.HIGH) #4
 		GPIO.output(6,GPIO.HIGH) #3
 		GPIO.output(13,GPIO.LOW) #2
 		GPIO.output(19,GPIO.LOW) #1
+		print "41to60"
 	if 21 <= rssi <= 40: #RSSI 3,2,1 off
 		GPIO.output(5,GPIO.HIGH) #4
 		GPIO.output(6,GPIO.LOW) #3
 		GPIO.output(13,GPIO.LOW) #2
 		GPIO.output(19,GPIO.LOW) #1
+		print "21to40"
 	if 0 <= rssi <= 20: #(no coverage/Very Poor)
 		GPIO.output(5,GPIO.HIGH) #4
 		GPIO.output(6,GPIO.LOW) #3
 		GPIO.output(13,GPIO.LOW) #2
 		GPIO.output(19,GPIO.HIGH) #1
+		print "nil"
 #Login to Nanostation
 def login():
 	ssl._create_default_https_context = ssl._create_unverified_context
