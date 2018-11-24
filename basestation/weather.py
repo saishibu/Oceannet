8162602145b6039554df907c0b8543dd62737e0c
Sai Shibu N B <saishibu38380@gmail.com>
2018-11-24 14:58:20 +0530

    Update weather.py

--- basestation/weather.py
+++ basestation/weather.py
@@ -7,7 +7,8 @@ from urllib.request import urlopen
 conn =pymysql.connect(database="micronet",user="on",password="amma",host="localhost")
 cur=conn.cursor()
 
-url="https://api.darksky.net/forecast/7a029bd49436a180466fbe67165cbf8e/9.092534,76.489965"
+#url="https://api.darksky.net/forecast/7a029bd49436a180466fbe67165cbf8e/9.092534,76.489965"
+url="https://api.darksky.net/forecast/c2576c4bc8dc2eba05e98617c2ebb6c2/9.092534,76.489965"
 api_page = urlopen(url)
 api=api_page.read()
 json_api=json.loads(api)
