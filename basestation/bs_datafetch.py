007049f4abff07cc79d8631c75e4d64bcfbcbbf3
Sai Shibu N B <saishibu38380@gmail.com>
2018-11-24 10:28:46 +0530

    Update bs_datafetch.py

--- basestation/bs_datafetch.py
+++ basestation/bs_datafetch.py
@@ -2,6 +2,8 @@
 import basestation as ns
 import time
 
+ip_bs1=192.168.179.100
+ip_bs1=192.168.179.66
 url_login_bs1='https://192.168.179.100/login.cgi'
 url_status_bs1='https://192.168.179.100/status.cgi'
 url_login_bs2='https://192.168.179.66/login.cgi'
@@ -13,16 +15,11 @@ print 'Login Success'
 a=1
 while a:
 	
-	signal1,rssi1,noise1,ccq1,distance1,devices1 = ns.fetchstatus(cj1,opener1,url_status_bs1)
-	signal2,rssi2,noise2,ccq2,distance2,devices2 = ns.fetchstatus(cj2,opener2,url_status_bs2)
+	data1 = ns.fetchstatus(cj1,opener1,url_status_bs1,ip_bs1)
+	data2 = ns.fetchstatus(cj2,opener2,url_status_bs2,ip_bs1)
 	print 'Data Fetch completed'
-	data1={'bs_ID':'100','ss':signal1,'nf':noise1,'rssi':rssi1,'ccq':ccq1,'d':distance1,'devices':devices1}
-	data2={'bs_ID':'66','ss':signal2,'nf':noise2,'rssi':rssi2,'ccq':ccq2,'d':distance2,'devices':devices2}
-	#data1={'bs_ID':'100','ss':signal1,'nf':noise1,'rssi':rssi1,'ccq':ccq1,'d':distance1}
-	#data2={'bs_ID':'66','ss':signal2,'nf':noise2,'rssi':rssi2,'ccq':ccq2,'d':distance2}
+	
 	print data1
 	print data2	
-	ns.todb(data1)
-	ns.todb(data2)
-	#time.sleep(300)
+
 	a=0
