40cc1e9ce04985f4a29fe16b95b140e7e944df29
Sai Shibu N B <saishibu38380@gmail.com>
2018-12-01 12:32:21 +0530

    Update basestation.py

--- basestation/basestation.py
+++ basestation/basestation.py
@@ -2,11 +2,14 @@ import urllib, urllib2, cookielib
 import ssl,json,time
 import pymysql
 
-def todb(data):
+def todb(data,dev):
 	conn =pymysql.connect(database="micronet",user="on",password="amma",host="localhost")
 	cur=conn.cursor()
 	#cur.execute("INSERT INTO basestation(bs_ID, ss, nf, ccq, d, rssi, devices) VALUES(%(bs_ID)s,%(ss)s,%(nf)s,%(ccq)s,%(d)s,%(rssi)s),%(devices)s);",data)
-	cur.execute("INSERT INTO bsparam(rcvdsgnl,ccq,distance,frequency,channel,noisefloor,txrate,rxrate,quality,capacity,deviceIp,devices) VALUES(%(rcvdsgnl)s,%(ccq)s,%(distance)s,%(frequency)s,%(channel)s,%(noisefloor)s,%(txrate)s,%(rxrate)s,%(quality)s,%(capacity)s,%(deviceIp)s,%(devices)s);",data)
+	if dev=1:
+		cur.execute("INSERT INTO bsparam(rcvdsgnl,ccq,distance,frequency,channel,noisefloor,txrate,rxrate,quality,capacity,deviceIp,devices) VALUES(%(rcvdsgnl)s,%(ccq)s,%(distance)s,%(frequency)s,%(channel)s,%(noisefloor)s,%(txrate)s,%(rxrate)s,%(quality)s,%(capacity)s,%(deviceIp)s,%(devices)s);",data)
+	elif dev=2:
+		cur.execute("INSERT INTO clientparam(ip,name,mac,rx,tx,signalst,ccq,noisefloor) VALUES (%(ip)s,%(name)s,%(mac)s,%(rx)s,%(tx)s,%(signal)s,%(ccq)s,%(noisefloor)s;",data)
 	conn.commit()
 	conn.close()
 
@@ -39,7 +42,7 @@ def fetchstatus(cj,opener,url,ip_bs):
 	deviceIp=ip_bs
 	
 	data= {"rcvdsgnl":rcvdsgnl,"ccq":ccq,"distance":distance,"capacity":capacity,"frequency":frequency,"channel":channel,"noisefloor":noisefloor,"quality":quality,"txrate":txrate,"rxrate":rxrate,"devices":devices,"deviceIp":deviceIp} 
-	todb(data)
+	todb(data,1)
 	return data
 
 
@@ -52,4 +55,6 @@ def clientlist(cj,opener,url,ip_bs):
 	else:
 		json_client=json_client[0]
 		data={"mac":json_client['mac'],"ip":json_client['lastip'],"ip":json_client['lastip'],"signal":json_client['signal'],"ccq":json_client['ccq'],"name":json_client['name'],"noisefloor":json_client['noisefloor'],"tx":json_client['tx'],"rx":json_client['rx']}
+		
 		print(data)
+		todb(data,2)
