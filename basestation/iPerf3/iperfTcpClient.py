3dfd79f19ba5acb54d187db1919159e1da1b952e
Dhanesh Raj <imdhaneshraj@gmail.com>
2018-12-06 10:16:19 +0530

    Create iperfTcpClient.py

---
+++ basestation/iPerf3/iperfTcpClient.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+import iperf3
+import json,time
+import pymysql
+
+conn =pymysql.connect(database="micronet",user="on",password="amma",host="localhost")
+cur=conn.cursor()
+
+client = iperf3.Client()
+client.duration = 1
+client.server_hostname = '127.0.0.1'
+client.port = 5001
+client.protocol = 'udp'
+client.blksize = 2000
+client.num_streams = 10
+client.reverse = True
+
+print('Connecting to {0}:{1}'.format(client.server_hostname, client.port))
+result = client.run()
+
+if result.error:
+	print(result.error)
+else:
+	
+	data= {"protocol":str(client.protocol),"txBytes":result.bytes,"jitter":result.jitter_ms,"avgCpuLoad":result.local_cpu_total,"MB_s":result.MB_s,"mbps":result.Mbps,"deviceIp":client.server_hostname} 
+	print('Success Fetch Status')
+	print(data)
+	cur.execute("INSERT INTO iperf(protocol,txBytes,jitter,avgCpuLoad,MB_s,mbps,deviceIp) VALUES(%(protocol)s,%(txBytes)s,%(jitter)s,%(avgCpuLoad)s,%(MB_s)s,%(mbps)s,%(deviceIp)s);",data)
+	conn.commit()
+	conn.close()
