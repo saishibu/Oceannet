a2397797f776aac08fcdbce219221ccdd7012ee7
Dhanesh Raj <imdhaneshraj@gmail.com>
2018-12-06 10:49:36 +0530

    Update iperfclient.py

--- basestation/iPerf3/iperfclient.py
+++ basestation/iPerf3/iperfclient.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
 
 import iperf3
-import json,time
-import pymysql
+import json
 
-conn =pymysql.connect(database="micronet",user="on",password="amma",host="localhost")
-cur=conn.cursor()
 
 client = iperf3.Client()
-client.duration = 1
+client.duration = 2
 client.server_hostname = '127.0.0.1'
 client.port = 5001
-client.protocol = 'udp'
+client.protocol = 'tcp'
 client.blksize = 2000
 client.num_streams = 10
 client.reverse = True
@@ -23,9 +20,7 @@ if result.error:
 	print(result.error)
 else:
 	
-	data= {"protocol":str(client.protocol),"txBytes":result.bytes,"jitter":result.jitter_ms,"avgCpuLoad":result.local_cpu_total,"MB_s":result.MB_s,"mbps":result.Mbps,"deviceIp":client.server_hostname} 
+	//data= {"protocol":str(client.protocol),"txBytes":result.bytes,"jitter":result.jitter_ms,"avgCpuLoad":result.local_cpu_total,"MB_s":result.MB_s,"mbps":result.Mbps,"deviceIp":client.server_hostname} 
 	print('Success Fetch Status')
 	print(data)
-	cur.execute("INSERT INTO iperf(protocol,txBytes,jitter,avgCpuLoad,MB_s,mbps,deviceIp) VALUES(%(protocol)s,%(txBytes)s,%(jitter)s,%(avgCpuLoad)s,%(MB_s)s,%(mbps)s,%(deviceIp)s);",data)
-	conn.commit()
-	conn.close()
+	
