08592ba4bab617f3ae90b210ff812b9c837a7d8d
Sai Shibu N B <saishibu38380@gmail.com>
2019-02-02 15:00:24 +0530

    Update testStepper.py

--- stepper/testStepper.py
+++ stepper/testStepper.py
@@ -19,12 +19,11 @@ halfstep_seq = [
   [0,0,1,0],
   [0,0,0,0]
 ]
-for i in range(512):
+for i in range(1024):
   for halfstep in range(8):
     for pin in range(4):
       GPIO.output(control_pins[pin], halfstep_seq[halfstep][pin])
-      print(halfstep)
-      print(pin)
-    time.sleep(0.001)
+      print(halfstep_seq[halfstep][pin])
+    time.sleep(0.1)
 GPIO.cleanup()
 print("test completed")
