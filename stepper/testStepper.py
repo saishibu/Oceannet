81555d01b01f700d16b2b8ff316945d20c1f9cc8
Sai Shibu N B <saishibu38380@gmail.com>
2019-02-02 12:25:53 +0530

    Update testStepper.py

--- stepper/testStepper.py
+++ stepper/testStepper.py
@@ -1,3 +1,27 @@
 #!/usr/bin/python
 
 print('initialising stepper control...')
+
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+control_pins = [6,13,19,26]
+for pin in control_pins:
+  GPIO.setup(pin, GPIO.OUT)
+  GPIO.output(pin, 0)
+halfstep_seq = [
+  [1,0,0,0],
+  [1,1,0,0],
+  [0,1,0,0],
+  [0,1,1,0],
+  [0,0,1,0],
+  [0,0,1,1],
+  [0,0,0,1],
+  [1,0,0,1]
+]
+for i in range(512):
+  for halfstep in range(8):
+    for pin in range(4):
+      GPIO.output(control_pins[pin], halfstep_seq[halfstep][pin])
+    time.sleep(0.001)
+GPIO.cleanup()
