83e9c3cc2aa6e6ef56242a260439119b31d9ee47
Sai Shibu N B <saishibu38380@gmail.com>
2019-02-02 14:58:05 +0530

    Update testStepper.py

--- stepper/testStepper.py
+++ stepper/testStepper.py
@@ -10,14 +10,14 @@ for pin in control_pins:
   GPIO.setup(pin, GPIO.OUT)
   GPIO.output(pin, 0)
 halfstep_seq = [
-  [1,0,0,1],
-  [1,0,0,0],
-  [1,1,0,0],
-  [0,1,0,0],
-  [0,1,1,0],
+  [0,0,0,1],
   [0,0,1,0],
-  [0,0,1,1],
-  [0,0,0,1]
+  [0,1,0,0],
+  [1,0,0,0],
+  [0,1,0,0],
+  [0,0,1,0],
+  [0,0,1,0],
+  [0,0,0,0]
 ]
 for i in range(512):
   for halfstep in range(8):
