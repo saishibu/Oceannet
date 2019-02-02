f6e88127a791d48761309a2a691f9881d20f03eb
Sai Shibu N B <saishibu38380@gmail.com>
2019-02-02 15:11:59 +0530

    Create stepper_man.py

---
+++ stepper/stepper_man.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+print('initialising stepper control...')
+
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+control_pins = [6,13,19,26]
+for pin in control_pins:
+  GPIO.setup(pin, GPIO.OUT)
+  GPIO.output(pin, 0)
+for i in range(360)
+  for pin in control_pins:
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, 1)
+    time.sleep(0.01)
+    GPIO.output(pin, 1
+    time.sleep(0.01)
+GPIO.cleanup()
+print("test completed")
