4b607ca05726d805b6ea04122597af1961f5e903
Sai Shibu N B <saishibu38380@gmail.com>
2019-02-02 16:58:37 +0530

    Update testStepper.py

--- stepper/testStepper.py
+++ stepper/testStepper.py
@@ -6,6 +6,7 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
 control_pins = [6,13,19,26]
+control_pins2 = [26,19,13,6]
 for pin in control_pins:
   GPIO.setup(pin, GPIO.OUT)
   GPIO.output(pin, 0)
@@ -19,11 +20,16 @@ halfstep_seq = [
   [0,0,1,1],
   [0,0,0,1]
 ]
-for i in range(360):
+for i in range(100):
   for halfstep in range(8):
     for pin in range(4):
-      GPIO.output(control_pins[pin], halfstep_seq[halfstep][pin])
-      print(control_pins[pin])
-    time.sleep(0.01)
+      print(i)
+      if i<=30:
+        GPIO.output(control_pins[pin], halfstep_seq[halfstep][pin])
+        time.sleep(0.01)
+      else:
+        GPIO.output(control_pins2[pin], halfstep_seq[halfstep][pin])
+        time.sleep(0.01)
+            
 GPIO.cleanup()
 print("test completed")
