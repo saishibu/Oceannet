2f52e58061a6c6f94953f87d80aff516e9dc2245
Sai Shibu N B <saishibu38380@gmail.com>
2019-05-09 10:12:00 +0530

    Update nanostation.py

--- AAR/nanostation.py
+++ AAR/nanostation.py
@@ -134,6 +134,7 @@ def fetchip(cj,opener,ip):
 	sta_page=opener.open(url)
 	sta=sta_page.read()
 	json_sta=json.loads(sta)
+	print(json_sta)
 	bs_ip=json_sta[0]['lastip']
 	ping_ms=json_sta['0']['tx_latency']
 	return bs_ip,ping_ms
