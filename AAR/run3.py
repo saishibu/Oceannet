b7063f1b0b3a1833e7c26b01846292f43a404833
Sai Shibu N B <saishibu38380@gmail.com>
2019-09-16 09:31:38 +0530

    Add files via upload

---
+++ AAR/run3.py
@@ -0,0 +1,56 @@
+import FaBo9Axis_MPU9250
+import time
+import sys,math
+
+mpu9250 = FaBo9Axis_MPU9250.MPU9250()
+
+try:
+    while True:
+        accel = mpu9250.readAccel()
+        print(" ax = " , ( accel['x'] ))
+        print(" ay = " , ( accel['y'] ))
+        print(" az = " , ( accel['z'] ))
+
+        gyro = mpu9250.readGyro()
+        print(" gx = " , ( gyro['x'] ))
+        print(" gy = " , ( gyro['y'] ))
+        print(" gz = " , ( gyro['z'] ))
+
+        mag = mpu9250.readMagnet()
+        print(" mx = " , ( mag['x'] ))
+        print(" my = " , ( mag['y'] ))
+        print(" mz = " , ( mag['z'] ))
+        print("\n")
+	magAngle=math.atan2(mag['y'],mag['x'])
+	print(magAngle)
+	if (magAngle < 0):
+		magAngle += 2*22/7
+	elif (magAngle >2*22/7):
+		magAngle-=2*22/7
+	magAngle=magAngle*180/22*7
+	print(magAngle)
+	if (magAngle >=0 and magAngle <=22):
+		dir="N"
+	if(magAngle >=23 and magAngle <=68):
+		dir='NE'
+	if(magAngle >=69 and magAngle <=112):
+                dir='E'
+	if(magAngle >=113 and magAngle <=156):
+                dir='SE'
+	if(magAngle >=157 and magAngle <=202):
+                dir='S'
+	if(magAngle >=203 and magAngle <=245):
+                dir='SW'
+	if(magAngle >=246 and magAngle <=291):
+                dir='W'
+	if(magAngle >=292 and magAngle <=337):
+                dir='NW'
+	if(magAngle >=338 and magAngle <=360):
+                dir='N'
+	print(dir)
+	data={'Ax':accel['x'],'Ay':accel['y'],'Az':accel['z'],'Gx':gyro['x'],'Gy':gyro['y'],'Gz':gyro['z'],'Mx':mag['x'],'My':mag['y'],'Mz':mag['z'],"Dir":dir}
+	print(data)
+	time.sleep(0.1)	
+
+except KeyboardInterrupt:
+    sys.exit()
