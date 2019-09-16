7fcc0114036fc0388f5718dda16c8073ec6a8a05
Sai Shibu N B <saishibu38380@gmail.com>
2019-09-16 09:59:13 +0530

    Update seaState.py

--- AAR/seaState.py
+++ AAR/seaState.py
@@ -53,7 +53,8 @@ try:
 	#print(dir)
 	data={'magAngle':magAngle,'Ax':accel['x'],'Ay':accel['y'],'Az':accel['z'],'Gx':gyro['x'],'Gy':gyro['y'],'Gz':gyro['z'],'Mx':mag['x'],'My':mag['y'],'Mz':mag['z'],"Dir":dir}
 	print(data)
-	cur.execute("INSERT INTO seastate (Ax, Ay, Az, Gx, Gy, Gz, Mx, My, Mz, Dir, magAngle) VALUES ((%Ax), (%Ay), (%Az), (%Gx), (%Gy), (%Gz), (%Mx), (%My), (%Mz), (%Dir), (%magAngle));",data)	time.sleep(0.1)	
+	cur.execute("INSERT INTO seastate (Ax, Ay, Az, Gx, Gy, Gz, Mx, My, Mz, Dir, magAngle) VALUES ((%Ax), (%Ay), (%Az), (%Gx), (%Gy), (%Gz), (%Mx), (%My), (%Mz), (%Dir), (%magAngle));",data)	
+	time.sleep(0.1)	
 
 except KeyboardInterrupt:
     sys.exit()
