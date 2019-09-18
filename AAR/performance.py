c745066b48cbf0eb759fba8117f243d7fea1d329
Sai Shibu N B <saishibu38380@gmail.com>
2019-09-18 14:42:43 +0530

    Update performance.py

--- AAR/performance.py
+++ AAR/performance.py
@@ -10,7 +10,6 @@ try:
     t =datetime.datetime.now()
     unix_secs = int(mktime(t.timetuple()))
 
-    print("Temperature")
 
     try:
       temp=subprocess.check_output("vcgencmd measure_temp | awk 'NR==1 {print $1}'", shell=True)
@@ -18,14 +17,10 @@ try:
     except:
       temp=int(subprocess.check_output("cat /sys/class/thermal/thermal_zone0/temp", shell=True))
       temp=temp/1000  
-    print(temp)
 
-    print("Available-RAM")
     RAM = psutil.virtual_memory()
     RAM=RAM[1]/(1024*1024)
-    print(RAM)
 
-    print("CPU usage in %")
     CPU=psutil.cpu_percent(interval=None)
     CPU=float(CPU)
     CPUFreq=psutil.cpu_freq()
@@ -39,6 +34,7 @@ try:
     data={'time':unix_secs,'temp':temp,'CPU':CPU,'RAM':RAM,'disk':disk,'CPUCurrent':CPUCurrent,'CPUMin':CPUMin,'CPUMax':CPUMax}
     cur.execute("INSERT INTO performance (timestamp,temp,RAM,CPU,disk,CPUCurrent,CPUMax,CPUMin) VALUES (%(time)s,%(temp)s,%(RAM)s,%(CPU)s,%(disk)s,%(CPUCurrent)s,%(CPUMin)s,%(CPUMax)s);",data)
     conn.commit()
+    print(data)
     print("Committed")
     time.sleep(120)
 
