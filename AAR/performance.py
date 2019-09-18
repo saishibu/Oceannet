1450b31642869bab6567ddbbf9275279e9e095ed
Sai Shibu N B <saishibu38380@gmail.com>
2019-09-18 15:09:55 +0530

    Update performance.py

--- AAR/performance.py
+++ AAR/performance.py
@@ -37,7 +37,8 @@ try:
     netIO=psutil.net_io_counters()
     
     data={'time':unix_secs,'temp':temp,'CPU':CPU,'RAM':RAM,'disk':disk,'CPUCurrent':CPUCurrent,'CPUMin':CPUMin,'CPUMax':CPUMax,'loadAvg1':loadAvg1,'loadAvg5':loadAvg5,'loadAvg15':loadAvg15,'bytes_sent':netIO[0],'bytes_recv':netIO[1],'packets_sent':netIO[2],'packets_recv':netIO[3],'errin':netIO[4],'errout':netIO[5],'dropin':netIO[6],'dropout':netIO[7]}
-    cur.execute("INSERT INTO performance (timestamp,temp,RAM,CPU,disk,CPUFreqCurrent,CPUFreqMax,CPUFreqMin,loadAvg1,loadAvg5,loadAvg15,bytes_sent,bytes_recv,packets_sent,packets_recv,errin,errout,dropin,dropout) VALUES (%(time)s,%(temp)s,%(RAM)s,%(CPU)s,%(disk)s,%(CPUCurrent)s,%(CPUMin)s,%(CPUMax)s,%(loadAvg1)s,%(loadAvg5)s,%(loadAvg15)s,%(bytes_sent)s,%(bytes_recv)s,%(packets_sent)s,%(packets_recv)s,%(errin)s,%(errout)s,%(dropin)s,%(dropout)s);",data)    conn.commit()
+    cur.execute("INSERT INTO performance (timestamp,temp,RAM,CPU,disk,CPUFreqCurrent,CPUFreqMax,CPUFreqMin,loadAvg1,loadAvg5,loadAvg15,bytes_sent,bytes_recv,packets_sent,packets_recv,errin,errout,dropin,dropout) VALUES (%(time)s,%(temp)s,%(RAM)s,%(CPU)s,%(disk)s,%(CPUCurrent)s,%(CPUMin)s,%(CPUMax)s,%(loadAvg1)s,%(loadAvg5)s,%(loadAvg15)s,%(bytes_sent)s,%(bytes_recv)s,%(packets_sent)s,%(packets_recv)s,%(errin)s,%(errout)s,%(dropin)s,%(dropout)s);",data)    
+    conn.commit()
     print(data)
     print("Committed")
     time.sleep(120)
