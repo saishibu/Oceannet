f33bf3625a141cec3dd43d4800bd923f07b82e99
Sai Shibu N B <saishibu38380@gmail.com>
2019-10-09 15:48:33 +0530

    Update generateConfig.py

--- AAR/generateConfig.py
+++ AAR/generateConfig.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+import pymysql
 
 boatName=raw_input("Enter Boat Name: ")
 cpeIP=raw_input("Enter CPE IP: ")
@@ -8,7 +9,10 @@ print("Validate the configurations")
 print(data)
 ans=raw_input("Store Data (Y/N): ")
 if ans == "Y":
-  print("Success")
+  conn =pymysql.connect(database="autosys",user="on",password="amma",host="localhost")
+	cur=conn.cursor()
+	cur.execute("INSERT INTO boat_data (boatName, cpeIP) VALUES (%(boatName)s, %(cpeIP)S);",data)
+	print("Success")
 elif ans == "N":
   print("Config Ignored")
 else:
