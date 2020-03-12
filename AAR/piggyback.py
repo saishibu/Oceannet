f4fd8f6e7ba1cefbe6c856070634101efc905805
Sai Shibu N B <saishibu38380@gmail.com>
2020-03-12 11:02:48 +0530

    Create piggyback.py

---
+++ AAR/piggyback.py
@@ -0,0 +1,8 @@
+import paho.mqtt.client as mqtt
+
+# This is the Publisher
+def helper(data)
+  client = mqtt.Client()
+  client.connect("103.10.24.222",1883,60)
+  client.publish("SGM/onpiggyback",data);
+  client.disconnect();
